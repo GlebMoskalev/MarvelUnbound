9448ededcfc57772fbc6bcb4147a002757e4f4fd
GlebMoskalev <gleb12122003@gmail.com>
2024-11-02 12:33:13 +0500

    Initial Commit

---
+++ MarvelUnbound/MarvelUnboundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelUnboundApp.swift
+//  MarvelUnbound
+//
+//  Created by Глеб Москалев on 02.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MarvelUnboundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
